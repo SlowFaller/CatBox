f406b84c4d3e2cbef5c37452d83009c02d41e437
MortalCoil <stupicide@hotmail.com>
2020-10-03 08:57:16 -0700

    Adding all untracked files, will probably have conflicts in merge.

---
+++ Assets/1_Scripts/UI/ExitButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // quits the game
+    public void OnClick()
+    {
+        Debug.Log("Quitting the game");
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+}
---
+++ Assets/1_Scripts/UI/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // load's Jake's scene
+    public void OnClick()
+    {
+        Debug.Log("Starting game");
+        SceneManager.LoadScene(1);
+    }
+}
